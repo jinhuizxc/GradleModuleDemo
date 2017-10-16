0469392cf5ccbb8b04fef5f607554fe0d9115d17
JinHui <1004260403@qq.com>
2017-10-16 12:38:27 +0800

    创建2个module

--- app/src/main/java/com/example/jh/gradlemoduledemo/MainActivity.java
+++ app/src/main/java/com/example/jh/gradlemoduledemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.jh.gradlemoduledemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 多module 优化gradle 项目！
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
